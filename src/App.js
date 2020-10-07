f4ce24a207beb0d7ca1b4a85eb161e0cc7fc3ab1
Tsheten Renzee Sherpa <trex33160@gmail.com>
2020-10-07 22:57:55 +0545

    added the layout and snake component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 function App() {
+  const [state, setState] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game-area">
+      <div className="snake-dot" style={{ top: 0, left: 0 }}></div>
+      <div className="snake-dot" style={{ top: 0, left: "2%" }}></div>
+      <div className="snake-dot" style={{ top: 0, left: "4%" }}></div>
     </div>
   );
 }
